9a87a345eb3817c4fcf4b322a1567055d781d355
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-10-15 14:54:49 +0100

    Run commands in the UTS namespace
    Co-authored-by: Maya Rosecrance <mrosecrance@pivotal.io>

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
 	cmd.Stdout = os.Stdout
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS,
+	}
 
 	err := cmd.Run()
 	if err != nil {
--- main_test.go
+++ main_test.go
@@ -25,4 +25,33 @@ var _ = Describe("Main", func() {
 		Expect(err).NotTo(HaveOccurred())
 		Expect(session.Out).To(gbytes.Say("bye, world!"))
 	})
+
+	Context("hostname isolation", func() {
+		var systemHostname string
+
+		BeforeEach(func() {
+			command := exec.Command("hostname")
+			session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit())
+			systemHostname = string(session.Out.Contents())
+		})
+
+		It("runs the command in its own UPS namespace", func() {
+			cmd := exec.Command("sudo", hellPath, "bash", "-c", "hostname foo; hostname")
+
+			session, err := gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+
+			Eventually(session).Should(gexec.Exit())
+			Expect(err).NotTo(HaveOccurred())
+			Expect(session.Out).To(gbytes.Say("foo"))
+
+			command := exec.Command("hostname")
+			session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+			Eventually(session).Should(gexec.Exit())
+
+			Expect(session.Out).To(gbytes.Say(systemHostname))
+		})
+	})
 })
