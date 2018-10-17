5023c3aa32decd1e0ca4addb865ab0bbc86fe5cb
Maya Rosecrance <mrosecrance@pivotal.io>
2018-10-17 12:09:13 +0100

    Add support for providing a rootfs for the container
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- container-run/main.go
+++ container-run/main.go
@@ -24,7 +24,7 @@ func parent() {
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWNS,
 	}
 
 	err := cmd.Run()
@@ -35,10 +35,16 @@ func parent() {
 
 func child() {
 	var cpusetName string
+	var rootfsPath string
 
 	flagSet := flag.NewFlagSet("child", flag.ExitOnError)
 	flagSet.StringVar(&cpusetName, "cpuset", "", "the cpuset name")
+	flagSet.StringVar(&rootfsPath, "rootfs", "", "the rootfs path")
 	flagSet.Parse(os.Args[2:])
+
+	if rootfsPath == "" {
+		panic("-rootfs is Required")
+	}
 
 	cmd := exec.Command(flagSet.Args()[0], flagSet.Args()[1:]...)
 	cmd.Stdout = os.Stdout
@@ -48,9 +54,21 @@ func child() {
 	pid := os.Getpid()
 	ioutil.WriteFile(filepath.Join("/sys/fs/cgroup/cpuset", cpusetName, "tasks"), []byte(strconv.Itoa(pid)), 0755)
 
+	oldRootfsPath := filepath.Join(rootfsPath, "oldrootfs")
+	must(syscall.Mount(rootfsPath, rootfsPath, "", syscall.MS_BIND, ""))
+	must(os.MkdirAll(oldRootfsPath, 0700))
+	must(syscall.PivotRoot(rootfsPath, oldRootfsPath))
+	must(os.Chdir("/"))
+
 	err := cmd.Run()
 	if err != nil {
 		exitWithError(err)
+	}
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
 
