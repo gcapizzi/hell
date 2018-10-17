49ba060c738c4f3c41f377005dfa3e6e3bebcd93
Maya Rosecrance <mrosecrance@pivotal.io>
2018-10-17 10:23:37 +0100

    Add nesting of processes
    - parent child processes
    - This allows us to run commands from different views of the system
    (namespaces)
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- container-run/main.go
+++ container-run/main.go
@@ -11,16 +11,41 @@ import (
 )
 
 func main() {
-	var cpusetName string
+	if os.Args[1] == "child" {
+		child()
+	} else {
+		parent()
+	}
+}
 
-	flag.StringVar(&cpusetName, "cpuset", "", "the cpuset name")
-	flag.Parse()
-
-	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
+func parent() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[1:]...)...)
 	cmd.Stdout = os.Stdout
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+
+	err := cmd.Run()
+	if err != nil {
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exitErr.Sys().(syscall.WaitStatus); ok {
+				os.Exit(status.ExitStatus())
+			}
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func child() {
+	var cpusetName string
+
+	flagSet := flag.NewFlagSet("child", flag.ExitOnError)
+	flagSet.StringVar(&cpusetName, "cpuset", "", "the cpuset name")
+	flagSet.Parse(os.Args[2:])
+
+	cmd := exec.Command(flagSet.Args()[0], flagSet.Args()[1:]...)
+	cmd.Stdout = os.Stdout
 
 	pid := os.Getpid()
 	ioutil.WriteFile(filepath.Join("/sys/fs/cgroup/cpuset", cpusetName, "tasks"), []byte(strconv.Itoa(pid)), 0755)
